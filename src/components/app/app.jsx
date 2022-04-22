405e21274cdb0e73d74ed7d1334b3d3f46036455
Samvel <samvel.bro@mail.ru>
2022-04-22 18:18:12 +0200

    made a repository

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,27 +1,16 @@
 import React, { useState } from 'react';
-import Header from '../header/header';
-import Content from '../content/content';
-import "./app.scss";
+import styles from "./app.module.scss";
 
-const App = () => {
-    let [counter, setCounter] = useState(4);
+const Button = (props) => {
+    return (
+        <button>{props.children}</button>
+    )
+};
 
-    const buttons = [
-        {id: 1, text: "Add Card", data: "add"},
-        {id: 2, text: "Del Card", data: "del"},
-    ]
-
-    const [data, setData] = useState([
-        {id: 1, q : "How old are you?", a : "20"},
-        {id: 2, q : "Are you married?", a : "No"},
-        {id: 3, q : "Do you know English?", a : "Yes"},
-    ])
+const App = (e) => {
 
     return (
-        <div className="flashcards">
-            <Header buttons={buttons} setData={setData}/>
-            <Content data={data} setData={setData} counter={counter} setCounter={setCounter}/>
-        </div>
+        <Button>1</Button>
     );
 }
  
